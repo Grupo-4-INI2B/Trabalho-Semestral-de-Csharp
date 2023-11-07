99e1d6875fe4372d65ef6d39df46649115b26e2d
AyumiJuliana <144122203+AyumiJuliana@users.noreply.github.com>
2023-11-07 08:24:43 -0300

    correção sql

--- Banco de Dados/Script _BD.sql
+++ Banco de Dados/Script _BD.sql
@@ -16,7 +16,6 @@ CREATE TABLE Produto (
 CREATE TABLE Fornecedor (
     idFornecedor SERIAL NOT NULL PRIMARY KEY,
     nomeFornecedor VARCHAR(45) NOT NULL,
-    cpf VARCHAR(14) NOT NULL,
     telefone VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     endereco VARCHAR(45) NOT NULL
