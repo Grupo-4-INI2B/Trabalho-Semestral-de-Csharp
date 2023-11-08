541a07324e52fd6b995f90c16fb4f44b315d82c2
AyumiJuliana <144122203+AyumiJuliana@users.noreply.github.com>
2023-11-07 22:12:07 -0300

    att

--- trblh_semestral/trblh_semestral/FrmCadProd.Designer.cs
+++ trblh_semestral/trblh_semestral/FrmCadProd.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.LblAreaCadProd = new System.Windows.Forms.Label();
+            this.LblProduto = new System.Windows.Forms.Label();
+            this.LblCategoria = new System.Windows.Forms.Label();
+            this.LblDescrição = new System.Windows.Forms.Label();
+            this.LblDataCad = new System.Windows.Forms.Label();
+            this.LblValidade = new System.Windows.Forms.Label();
+            this.LblDisponivel = new System.Windows.Forms.Label();
+            this.LblQuantidade = new System.Windows.Forms.Label();
+            this.LblPreço = new System.Windows.Forms.Label();
+            this.LblFornecedor = new System.Windows.Forms.Label();
+            this.TxtProduto = new System.Windows.Forms.TextBox();
+            this.TxtCategoria = new System.Windows.Forms.TextBox();
+            this.TxtDescrição = new System.Windows.Forms.TextBox();
+            this.TxtFornecedor = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
             this.SuspendLayout();
             // 
             // LblAreaCadProd
@@ -36,15 +52,162 @@
             this.LblAreaCadProd.AutoSize = true;
             this.LblAreaCadProd.Location = new System.Drawing.Point(13, 13);
             this.LblAreaCadProd.Name = "LblAreaCadProd";
-            this.LblAreaCadProd.Size = new System.Drawing.Size(44, 16);
+            this.LblAreaCadProd.Size = new System.Drawing.Size(208, 16);
             this.LblAreaCadProd.TabIndex = 0;
-            this.LblAreaCadProd.Text = "label1";
+            this.LblAreaCadProd.Text = "Cadastro e Pesquisa de produtos";
+            // 
+            // LblProduto
+            // 
+            this.LblProduto.AutoSize = true;
+            this.LblProduto.Location = new System.Drawing.Point(38, 71);
+            this.LblProduto.Name = "LblProduto";
+            this.LblProduto.Size = new System.Drawing.Size(44, 16);
+            this.LblProduto.TabIndex = 1;
+            this.LblProduto.Text = "Nome";
+            // 
+            // LblCategoria
+            // 
+            this.LblCategoria.AutoSize = true;
+            this.LblCategoria.Location = new System.Drawing.Point(155, 71);
+            this.LblCategoria.Name = "LblCategoria";
+            this.LblCategoria.Size = new System.Drawing.Size(66, 16);
+            this.LblCategoria.TabIndex = 2;
+            this.LblCategoria.Text = "Categoria";
+            // 
+            // LblDescrição
+            // 
+            this.LblDescrição.AutoSize = true;
+            this.LblDescrição.Location = new System.Drawing.Point(272, 70);
+            this.LblDescrição.Name = "LblDescrição";
+            this.LblDescrição.Size = new System.Drawing.Size(69, 16);
+            this.LblDescrição.TabIndex = 3;
+            this.LblDescrição.Text = "Descrição";
+            // 
+            // LblDataCad
+            // 
+            this.LblDataCad.AutoSize = true;
+            this.LblDataCad.Location = new System.Drawing.Point(38, 214);
+            this.LblDataCad.Name = "LblDataCad";
+            this.LblDataCad.Size = new System.Drawing.Size(111, 16);
+            this.LblDataCad.TabIndex = 4;
+            this.LblDataCad.Text = "Data de cadastro";
+            // 
+            // LblValidade
+            // 
+            this.LblValidade.AutoSize = true;
+            this.LblValidade.Location = new System.Drawing.Point(287, 214);
+            this.LblValidade.Name = "LblValidade";
+            this.LblValidade.Size = new System.Drawing.Size(62, 16);
+            this.LblValidade.TabIndex = 5;
+            this.LblValidade.Text = "Validade";
+            // 
+            // LblDisponivel
+            // 
+            this.LblDisponivel.AutoSize = true;
+            this.LblDisponivel.Location = new System.Drawing.Point(624, 71);
+            this.LblDisponivel.Name = "LblDisponivel";
+            this.LblDisponivel.Size = new System.Drawing.Size(102, 16);
+            this.LblDisponivel.TabIndex = 6;
+            this.LblDisponivel.Text = "Disponibilidade";
+            // 
+            // LblQuantidade
+            // 
+            this.LblQuantidade.AutoSize = true;
+            this.LblQuantidade.Location = new System.Drawing.Point(38, 148);
+            this.LblQuantidade.Name = "LblQuantidade";
+            this.LblQuantidade.Size = new System.Drawing.Size(77, 16);
+            this.LblQuantidade.TabIndex = 7;
+            this.LblQuantidade.Text = "Quantidade";
+            // 
+            // LblPreço
+            // 
+            this.LblPreço.AutoSize = true;
+            this.LblPreço.Location = new System.Drawing.Point(155, 148);
+            this.LblPreço.Name = "LblPreço";
+            this.LblPreço.Size = new System.Drawing.Size(43, 16);
+            this.LblPreço.TabIndex = 8;
+            this.LblPreço.Text = "Preço";
+            // 
+            // LblFornecedor
+            // 
+            this.LblFornecedor.AutoSize = true;
+            this.LblFornecedor.Location = new System.Drawing.Point(272, 148);
+            this.LblFornecedor.Name = "LblFornecedor";
+            this.LblFornecedor.Size = new System.Drawing.Size(77, 16);
+            this.LblFornecedor.TabIndex = 9;
+            this.LblFornecedor.Text = "Fornecedor";
+            // 
+            // TxtProduto
+            // 
+            this.TxtProduto.Location = new System.Drawing.Point(41, 90);
+            this.TxtProduto.Name = "TxtProduto";
+            this.TxtProduto.Size = new System.Drawing.Size(100, 22);
+            this.TxtProduto.TabIndex = 10;
+            // 
+            // TxtCategoria
+            // 
+            this.TxtCategoria.Location = new System.Drawing.Point(158, 90);
+            this.TxtCategoria.Name = "TxtCategoria";
+            this.TxtCategoria.Size = new System.Drawing.Size(100, 22);
+            this.TxtCategoria.TabIndex = 11;
+            // 
+            // TxtDescrição
+            // 
+            this.TxtDescrição.Location = new System.Drawing.Point(275, 90);
+            this.TxtDescrição.Name = "TxtDescrição";
+            this.TxtDescrição.Size = new System.Drawing.Size(100, 22);
+            this.TxtDescrição.TabIndex = 12;
+            // 
+            // TxtFornecedor
+            // 
+            this.TxtFornecedor.Location = new System.Drawing.Point(275, 168);
+            this.TxtFornecedor.Name = "TxtFornecedor";
+            this.TxtFornecedor.Size = new System.Drawing.Size(100, 22);
+            this.TxtFornecedor.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(41, 234);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(290, 233);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 15;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(50, 181);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.domainUpDown1.TabIndex = 16;
+            this.domainUpDown1.Text = "domainUpDown1";
             // 
             // FrmCadProd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TxtFornecedor);
+            this.Controls.Add(this.TxtDescrição);
+            this.Controls.Add(this.TxtCategoria);
+            this.Controls.Add(this.TxtProduto);
+            this.Controls.Add(this.LblFornecedor);
+            this.Controls.Add(this.LblPreço);
+            this.Controls.Add(this.LblQuantidade);
+            this.Controls.Add(this.LblDisponivel);
+            this.Controls.Add(this.LblValidade);
+            this.Controls.Add(this.LblDataCad);
+            this.Controls.Add(this.LblDescrição);
+            this.Controls.Add(this.LblCategoria);
+            this.Controls.Add(this.LblProduto);
             this.Controls.Add(this.LblAreaCadProd);
             this.Name = "FrmCadProd";
             this.Text = "FrmCadProd";
@@ -56,5 +219,21 @@
         #endregion
 
         private System.Windows.Forms.Label LblAreaCadProd;
+        private System.Windows.Forms.Label LblProduto;
+        private System.Windows.Forms.Label LblCategoria;
+        private System.Windows.Forms.Label LblDescrição;
+        private System.Windows.Forms.Label LblDataCad;
+        private System.Windows.Forms.Label LblValidade;
+        private System.Windows.Forms.Label LblDisponivel;
+        private System.Windows.Forms.Label LblQuantidade;
+        private System.Windows.Forms.Label LblPreço;
+        private System.Windows.Forms.Label LblFornecedor;
+        private System.Windows.Forms.TextBox TxtProduto;
+        private System.Windows.Forms.TextBox TxtCategoria;
+        private System.Windows.Forms.TextBox TxtDescrição;
+        private System.Windows.Forms.TextBox TxtFornecedor;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
     }
 }
